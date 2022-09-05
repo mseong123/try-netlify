b6ee55f20e97bd641ed77df183d907bd341e6717
Mseong <mseong123@hotmail.com>
2022-09-05 18:12:09 +0800

    changed again

--- netlify/functions/mseong.js
+++ netlify/functions/mseong.js
@@ -5,6 +5,9 @@ const serverless = require('serverless-http');
 app.get('/api',function(req,res) {
   res.send('holla');
 })
+app.get('/',function(req,res) {
+  res.send('oiosajdojosjdioad');
+})
 
 
 module.exports.handler = serverless(app)
